a34d107ce303b53cf972ff0a5d6cd92948398f26
giansalex <giansalex@gmail.com>
2017-09-05 13:03:31 -0500

    Update nuget package

--- Sufel.Sync/nuget_build.bat
+++ Sufel.Sync/nuget_build.bat
@@ -3,4 +3,4 @@
 SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
 
 if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '.\nuget.exe')"
-.\nuget.exe pack Sufel.Sync.nuspec -properties Configuration=Release
+.\nuget.exe pack Sufel.Sync.csproj -properties Configuration=Release
