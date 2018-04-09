e3e9752fa33aa64c83696b60a26e7d1da6ef8efe
Giancarlos Salas <giansalex@gmail.com>
2018-04-08 21:38:34 -0500

    add build bat + new release

---
+++ nuget_build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
+
+if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '.\nuget.exe')"
+.\nuget.exe pack Sufel.Sync/Sufel.Sync.csproj -properties Configuration=Release
