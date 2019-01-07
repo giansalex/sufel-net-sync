47cf00c09c7e29554bb343553c713d2eea4e6e9b
Giancarlos Salas <giansalex@gmail.com>
2019-01-06 21:27:23 -0500

    add tests

---
+++ Sufel.Sync.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sufel.Sync.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sufel.Sync.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2117e91-a336-4f62-921a-5ed836b4587b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sufel.Sync.Tests/UploaderTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sufel.Sync.Tests
+{
+    [TestClass]
+    public class UploaderTest
+    {
+        [TestMethod]
+        public void UploadTest()
+        {
+            var uploader = new Uploader
+            {
+                Setting = new Model.SufelSetting
+                {
+                    Ruc = "20123456789",
+                    Password = "",
+                    Endpoint = ""
+                }
+            };
+        }
+    }
+}
